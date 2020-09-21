80eb7ec5e29431c3ed0d735bc16d275cf0554953
Julian Ibarra <yeissonibarra@gmail.com>
2020-09-21 11:38:03 +0100

    Updates

---
+++ precache-manifest.d45a230c8288b827536e8efccc88d568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a3c2c66f19d6b78b051b9fed018654",
+    "url": "/lusitania-theme/index.html"
+  },
+  {
+    "revision": "79ba430961e12ddb2532",
+    "url": "/lusitania-theme/static/css/main.bd25b512.chunk.css"
+  },
+  {
+    "revision": "0509f9d5a3aa74834015",
+    "url": "/lusitania-theme/static/js/2.515affcb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lusitania-theme/static/js/2.515affcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ba430961e12ddb2532",
+    "url": "/lusitania-theme/static/js/main.b04d55f5.chunk.js"
+  },
+  {
+    "revision": "0eb8174f526f1ca41cab",
+    "url": "/lusitania-theme/static/js/runtime-main.3793aae2.js"
+  }
+]);

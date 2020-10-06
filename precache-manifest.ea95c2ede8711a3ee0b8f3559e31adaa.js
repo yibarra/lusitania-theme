f8640b79fe6b5caf6fca2b23eab5d6a47e9e2f98
Julian Ibarra <yeissonibarra@gmail.com>
2020-10-06 13:57:04 +0100

    Updates

---
+++ precache-manifest.ea95c2ede8711a3ee0b8f3559e31adaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6447792f4ae7e3a586a0dc35820ed64a",
+    "url": "/lusitania-theme/index.html"
+  },
+  {
+    "revision": "af5aceceb989e77ba6ad",
+    "url": "/lusitania-theme/static/css/main.dd1d7add.chunk.css"
+  },
+  {
+    "revision": "94e94ce91ba066df203c",
+    "url": "/lusitania-theme/static/js/2.838e52b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lusitania-theme/static/js/2.838e52b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af5aceceb989e77ba6ad",
+    "url": "/lusitania-theme/static/js/main.86c93a4d.chunk.js"
+  },
+  {
+    "revision": "0eb8174f526f1ca41cab",
+    "url": "/lusitania-theme/static/js/runtime-main.3793aae2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lusitania-theme/precache-manifest.a215194ebcaaceeedc52e2214aa4bf29.js"
+  "/lusitania-theme/precache-manifest.ea95c2ede8711a3ee0b8f3559e31adaa.js"
 );
 
 self.addEventListener('message', (event) => {
